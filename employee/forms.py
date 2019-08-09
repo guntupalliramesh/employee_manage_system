d101791786e0b48821e6b277f05af0ca58bae90a
rameshguntupalli <44597824+guntupalliramesh@users.noreply.github.com>
2019-08-09 18:23:31 +0530

    Initial Commit

---
+++ employee/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User,Group
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    role = forms.ModelChoiceField(queryset=Group.objects.all())
+
+    class Meta:
+        model= User
+        fields = ['first_name','last_name',
+                    'email','username',
+                    'password']
+
+        label={
+            'password':'Password'
+        }
+
+    def __init__(self,*args, **kwargs):
+        if kwargs.get('instance'):
+            initial = kwargs.setdefault('initial',{})
+            if kwargs['instance'].groups.all():
+                initial['role'] = kwargs['instance'].groups.all()[0]
+            else:
+                initial['role'] = None
+        forms.ModelForm.__init__(self,*args,**kwargs)
+
+    # def cleaned_email(self):
+    #     if self.cleaned_data['email'].endsWith('@gmail.com'):
+    #         return self.cleaned_data['email']
+    #     else:
+    #         raise ValidationError('Email is Not Valid')
+
+    def save(self):
+        password = self.cleaned_data.pop('password')
+        role = self.cleaned_data.pop('role')
+        u = super().save()
+        u.groups.set([role])
+        u.set_password(password)
+        u.save()
+        return u
